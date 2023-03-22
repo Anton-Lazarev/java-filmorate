2504abfebdef296559c7f32fbcced3691abce87f
Anton-Lazarev <big.panci@yandex.ru>
2023-03-22 13:58:15 +0300

    fix : remove unnecessary annotation in MPA class

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,7 +8,6 @@ import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class MPA {
     private int id;
     @NotEmpty(message = "Наименование рейтинга не может быть пустым")
